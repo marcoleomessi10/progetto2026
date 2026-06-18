22d84ed979c20eedcd7f83259ec46e33ea211685
marcoleomessi10 <marcomanna2070@gmail.com>
2026-06-18 16:57:14 +0200

    Creazione model Category

---
+++ src/it/model/Category.java
@@ -0,0 +1,31 @@
+package it.model;
+
+public class Category {
+
+    private int id;
+    private String nome;
+    private String descrizione;
+
+    public Category() {}
+
+    public int getId() { 
+    	return id; 
+    	}
+    public void setId(int id) { 
+    	this.id = id; 
+    	}
+
+    public String getNome() { 
+    	return nome; 
+    	}
+    public void setNome(String nome) { 
+    	this.nome = nome; 
+    	}
+
+    public String getDescrizione() { 
+    	return descrizione; 
+    	}
+    public void setDescrizione(String descrizione) { 
+    	this.descrizione = descrizione;
+    	}
+}
