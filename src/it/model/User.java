35d6c88a9ab44723ec3022541902c3ad9e41d951
marcoleomessi10 <marcomanna2070@gmail.com>
2026-06-18 17:02:00 +0200

    Creazione model User

--- src/it/model/User.java
+++ src/it/model/User.java
@@ -1,5 +1,73 @@
 package it.model;
 
+import java.sql.Timestamp;
+
 public class User {
 
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String passwordHash;
+    private String telefono;
+    private String ruolo; 
+    private Timestamp dataRegistrazione;
+
+    public User() {}
+
+    public int getId() { 
+    	return id; 
+    	}
+    public void setId(int id) { 
+    	this.id = id; 
+    	}
+
+    public String getNome() { 
+    	return nome; 
+    	}
+    public void setNome(String nome) { 
+    	this.nome = nome; 
+    	}
+
+    public String getCognome() { 
+    	return cognome; 
+    	}
+    public void setCognome(String cognome) { 
+    	this.cognome = cognome; 
+    	}
+
+    public String getEmail() { 
+    	return email; 
+    	}
+    public void setEmail(String email) { 
+    	this.email = email; 
+    	}
+
+    public String getPasswordHash() { 
+    	return passwordHash; 
+    	}
+    public void setPasswordHash(String passwordHash) { 
+    	this.passwordHash = passwordHash;
+    	}
+
+    public String getTelefono() { 
+    	return telefono;
+    	}
+    public void setTelefono(String telefono) { 
+    	this.telefono = telefono; 
+    	}
+
+    public String getRuolo() { 
+    	return ruolo; 
+    	}
+    public void setRuolo(String ruolo) {
+    	this.ruolo = ruolo;
+    	}
+
+    public Timestamp getDataRegistrazione() { 
+    	return dataRegistrazione; 
+    	}
+    public void setDataRegistrazione(Timestamp dataRegistrazione) { 
+    	this.dataRegistrazione = dataRegistrazione;
+    	}
 }
