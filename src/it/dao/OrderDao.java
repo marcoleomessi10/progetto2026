d63660637e818d3b2a14ec69c6ceea29f7701fd2
Davide <davidemiele999@gmail.com>
2026-06-19 14:35:40 +0200

    Creazione orderDao.java

--- src/it/dao/OrderDao.java
+++ src/it/dao/OrderDao.java
@@ -1,5 +1,19 @@
 package it.dao;
 
-public class OrderDao {
+import java.util.List;
 
+import it.model.Order;
+import it.model.OrderItem;
+
+public interface OrderDao {
+
+    int doSaveOrder(Order order);
+
+    void doSaveOrderItem(OrderItem item);
+
+    List<Order> doRetrieveByUser(int userId);
+
+    List<Order> doRetrieveAll();
+
+    List<Order> doRetrieveByDateRange(String startDate, String endDate);
 }
