6e93388c66ae1b382fadb66037c2d5c768eb3551
marcoleomessi10 <marcomanna2070@gmail.com>
2026-06-26 08:42:33 +0200

    cambio immagini catalogo

--- db/storage.sql
+++ db/storage.sql
@@ -78,12 +78,22 @@ INSERT IGNORE INTO categories (id, name, description) VALUES
 (3, 'Eleganti', 'Scarpe eleganti');
 
 INSERT IGNORE INTO products (id, name, description, brand, price, category, stock, image, image_path, active) VALUES
-(1,  'Air Force One',     'Sneaker sportiva comoda e leggera',                    'Nike',         129.99, 'Sneakers', 10, 'air-runner.png',     'images/air-runner.png',     1),
-(2,  'Adidas Samba', 'Scarpa casual per tutti i giorni',                     'Adidas',        89.99, 'Sneakers', 15, 'street-classic.png', 'images/street-classic.png', 1),
-(3,  'Asics Gel',        'Scarpa tecnica da running',                            'Asics',        119.99, 'Running',   8, 'run-pro.png',        'images/run-pro.png',        1),
-(4,  'Geox Gladwin',  'Scarpa elegante nera in pelle',                        'Geox',         149.99, 'Eleganti',  6, 'elegant-black.png',  'images/elegant-black.png',  1),
-(5,  'Nike Dunk',  'Sneaker casual comoda per tutti i giorni',             'Nike',          89.90, 'Sneakers', 17, 'air-street-90.png',  'images/air-street-90.png',  1),
-(6,  'Adidas Adizero',    'Scarpa leggera pensata per la corsa su strada',        'Adidas',       119.50, 'Running',  12, 'runflex-pro.png',    'images/runflex-pro.png',    1),
-(7,  'Geox Spherica',  'Scarpa elegante in stile derby per occasioni formali', 'Geox',         139.00, 'Eleganti',  7, 'classic-derby.png',  'images/classic-derby.png',  1),
-(8,  'Puma Skyrocket',    'Sneaker urbana con suola ammortizzata',                'Puma',          74.99, 'Sneakers', 22, 'urban-pulse.png',    'images/urban-pulse.png',    1),
-(9,  'Asics Excite 11', 'Scarpa running traspirante con buon supporto',         'Asics',        129.90, 'Running',  10, 'marathon-light.png', 'images/marathon-light.png', 1);
+(1,  'Air Force One',     'Sneaker sportiva comoda e leggera',                    'Nike',         129.99, 'Sneakers', 10, 'air-force-one.png',  'images/air-force-one.png',     1),
+(2,  'Adidas Samba', 'Scarpa casual per tutti i giorni',                     'Adidas',        89.99, 'Sneakers', 15, 'adidas-samba.png', 'images/adidas-samba.png', 1),
+(3,  'Asics Gel',        'Scarpa tecnica da running',                            'Asics',        119.99, 'Running',   8, 'asics-gel.png',     'images/asics-gel.png',        1),
+(4,  'Geox Gladwin',  'Scarpa elegante nera in pelle',                        'Geox',         149.99, 'Eleganti',  6, 'geox-gladwin.png', 'images/geox-gladwin.png',  1),
+(5,  'Nike Dunk',  'Sneaker casual comoda per tutti i giorni',             'Nike',          89.90, 'Sneakers', 17, 'nike-dunk.png',    'images/nike-dunk.png',  1),
+(6,  'Adidas Adizero',    'Scarpa leggera pensata per la corsa su strada',        'Adidas',       119.50, 'Running',  12, 'adidas-adizero.png', 'images/adidas-adizero.png',    1),
+(7,  'Geox Spherica',  'Scarpa elegante in stile derby per occasioni formali', 'Geox',         139.00, 'Eleganti',  7, 'geox-spherica.png','images/geox-spherica.png',  1),
+(8,  'Puma Skyrocket',    'Sneaker urbana con suola ammortizzata',                'Puma',          74.99, 'Sneakers', 22, 'puma-skyrocket.png','images/puma-skyrocket.png',    1),
+(9,  'Asics Excite 11', 'Scarpa running traspirante con buon supporto',         'Asics',        129.90, 'Running',  10, 'asics-excite-11.png', 'images/asics-excite-11.png', 1);
+
+UPDATE products SET image = 'air-force-one.png', image_path = 'images/air-force-one.png' WHERE name = 'Air Force One';
+UPDATE products SET image = 'adidas-samba.png', image_path = 'images/adidas-samba.png' WHERE name = 'Adidas Samba';
+UPDATE products SET image = 'asics-gel.png', image_path = 'images/asics-gel.png' WHERE name = 'Asics Gel';
+UPDATE products SET image = 'geox-gladwin.png', image_path = 'images/geox-gladwin.png' WHERE name = 'Geox Gladwin';
+UPDATE products SET image = 'nike-dunk.png', image_path = 'images/nike-dunk.png' WHERE name = 'Nike Dunk';
+UPDATE products SET image = 'adidas-adizero.png', image_path = 'images/adidas-adizero.png' WHERE name = 'Adidas Adizero';
+UPDATE products SET image = 'geox-spherica.png', image_path = 'images/geox-spherica.png' WHERE name = 'Geox Spherica';
+UPDATE products SET image = 'asics-excite-11.png', image_path = 'images/asics-excite-11.png' WHERE name = 'Asics Excite 11';
+UPDATE products SET image = 'puma-skyrocket.png', image_path = 'images/puma-skyrocket.png' WHERE name = 'Puma Skyrocket';
